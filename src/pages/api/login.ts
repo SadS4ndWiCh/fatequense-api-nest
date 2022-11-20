76059e7e5e751529694c7460a5bcca7814f5b4cb
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-20 20:18:42 -0300

    add login siga api route

---
+++ src/pages/api/login.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { api } from './_libs/api';
+
+import { BTN_CONFIRM, PASSWORD_INPUT, STATUS, USERNAME_INPUT } from './_libs/network';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { username, password } = req.body;
+
+  const { res: responseLogin } = await api.post('login', {
+    [USERNAME_INPUT]: username,
+    [PASSWORD_INPUT]: password,
+    [BTN_CONFIRM]: 'Confirmar',
+  });
+
+  if (responseLogin.statusCode !== STATUS.redirect) {
+    return res.status(400).json({ error: 'Não foi possível logar na conta' });
+  }
+
+  return res.status(200).json({
+    cookie: String(responseLogin.headers['set-cookie']),
+  });
+}
