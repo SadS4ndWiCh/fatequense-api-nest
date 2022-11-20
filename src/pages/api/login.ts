1d296934c60c297ed5026b10f52383f797b49acc
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-20 20:40:32 -0300

    use auth siga schema

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { api } from './_libs/api';
 
+import { api } from './_libs/api';
 import { BTN_CONFIRM, PASSWORD_INPUT, STATUS, USERNAME_INPUT } from './_libs/network';
+import { authSigaBody } from './_libs/schemas';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { username, password } = req.body;
+  const { username, password } = authSigaBody.parse(req.body);
 
   const { res: responseLogin } = await api.post('login', {
     [USERNAME_INPUT]: username,
