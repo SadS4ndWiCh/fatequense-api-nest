4bc907c38301eb465dc24177d7037fb77678a663
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-22 18:18:04 -0300

    use rate limit in api route wrapper

--- src/pages/api/_libs/utils/api-route.ts
+++ src/pages/api/_libs/utils/api-route.ts
@@ -1,11 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export type RequestMethods = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'UPDATE' | 'PATCH';
-
-export type ApiRoute = (req: NextApiRequest, res: NextApiResponse) => Promise<any>;
-export type ApiRouteOptions = {
-	allowedMethods: RequestMethods[];
-}
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { ApiRoute, ApiRouteOptions, RequestMethods } from '../../../../@types/api-route';
+import { getIP, rateLimit } from './rate-limit';
 
 type Props = {
 	options?: ApiRouteOptions,
@@ -14,13 +9,26 @@ type Props = {
 
 const defaultOptions: ApiRouteOptions = {
 	allowedMethods: ['GET', 'POST'],
+	rateLimit: { requests: 10 }
 }
 
 export const withRouteOptions = ({
 	handler,
-	options = defaultOptions,
+	options = {} as ApiRouteOptions,
 }: Props) => {
+	options = { ...defaultOptions, ...options };
+	const limiter = options.rateLimit && rateLimit(options.rateLimit);
+
 	return async (req: NextApiRequest, res: NextApiResponse) => {
+		if (limiter) {
+			try {
+				const ip = getIP(req);
+				await limiter.check(res, options.rateLimit!.requests, ip);
+			} catch {
+				return res.status(429).json({ error: 'O limite de requisição foi excedido' });
+			}
+		}
+
 		if (req.method && !options.allowedMethods.includes(req.method.toUpperCase() as RequestMethods)) {
 			return res
 				.status(405)
