eeb480e24c8584535025f84d9667af0a5a4728fe
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-22 18:17:14 -0300

    add rate limit request utils function

---
+++ src/pages/api/_libs/utils/rate-limit.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import LRU from 'lru-cache';
+
+import type { RateLimitOptions } from '../../../../@types/rate-limit';
+
+export const getIP = (req: NextApiRequest) => {
+	const ip = req.headers['x-forwarded-for'] ||
+						 req.headers['x-real-ip'] ||
+						 req.socket.remoteAddress as string;
+	
+	return Array.isArray(ip) ? ip.join('') : ip;
+}	
+
+export const rateLimit = (options: RateLimitOptions) => {
+	const tokenCache = new LRU({
+		max: options?.uniqueTokenPerInterval || 250,
+		ttl: options?.interval || 60 * 1000 // 1 minuto
+	});
+
+	return {
+		check: (res: NextApiResponse, limit: number, token: string) => 
+			new Promise<void>((resolve, reject) => {
+				const tokenCount = (tokenCache.get(token) as number[]) || [0];
+				if (tokenCount[0] === 0) {
+					tokenCache.set(token, tokenCount);
+				}
+				tokenCount[0] += 1;
+
+				const currentUsage = tokenCount[0];
+				const isRateLimited = currentUsage >= limit;
+				res.setHeader('X-RateLimit-Limit', limit);
+				res.setHeader(
+					'X-RateLimit-Remaining',
+					isRateLimited ? 0 : limit - currentUsage,
+				);
+
+				return isRateLimited ? reject() : resolve();
+			})
+	}
+}
