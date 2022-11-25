52b9b7310c96c263e35181f47c3ddeb25d2f4f5d
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-25 17:42:57 -0300

    feat: format the student's name in title case

--- src/pages/api/_libs/scrappers/profile.scrap.ts
+++ src/pages/api/_libs/scrappers/profile.scrap.ts
@@ -1,12 +1,16 @@
 import { getterGXStateWithPrefix, IGetGXStateReturn } from "../utils/gxstate";
+import { toTitleCase } from "../utils/string-formatter";
 
 export const getProfile = ({ $, ...gxstate }: IGetGXStateReturn) => {
 	const gxstateGet = getterGXStateWithPrefix(gxstate.prefix!, gxstate.parsed);
 
+	let name = gxstate.parsed['vPRO_PESSOALNOME'];
+	name = toTitleCase(name.substring(0, name.length - 2));
+
 	const photoUrl = $(`#${gxstate.prefix}FOTO > img`).attr()?.src;
 
 	const profile: IProfile = {
-		name: gxstate.parsed['vPRO_PESSOALNOME'],
+		name,
 		personalEmail: gxstate.parsed['vPRO_PESSOALEMAIL'],
 		institutionalEmail: gxstateGet('vINSTITUCIONALFATEC'),
 		cpf: gxstate.parsed['vPRO_PESSOALDOCSCPF'],
