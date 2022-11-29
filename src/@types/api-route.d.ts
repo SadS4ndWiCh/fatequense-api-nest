c67145284584759a7a9dc3a0ea71f86b40ae4ddc
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-29 08:27:05 -0300

    chore: add api route validation types

--- src/@types/api-route.d.ts
+++ src/@types/api-route.d.ts
@@ -1,12 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
 declare global {
 	type RequestMethods = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'UPDATE' | 'PATCH';
 
 	type ApiRoute = (req: NextApiRequest, res: NextApiResponse) => Promise<any>;
+	type ApiValidationCallback = (req: NextApiRequest) => boolean;
+	type ApiValidation = {
+		fn: ApiValidationCallback;
+		errorMessage?: string;
+	}
 	type ApiRouteOptions = {
 		allowedMethods: RequestMethods[];
 		rateLimit?: RateLimitOptions & {
 			requests: number;
 		};
+		validations?: ApiValidation[];
 	}
 }
 
