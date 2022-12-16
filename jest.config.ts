a20559f8a57f91e5b8eb58470c8f285f1736cbe4
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-12-16 15:14:10 -0300

    chore: change jest configurations

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,20 @@
-import type { Config } from 'jest';
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
-const jestConfigs: Config = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	verbose: true,
-	transform: {
-		'^.+\\.ts?$': 'ts-jest',
-	},
-	transformIgnorePatterns: ['./node_modules/', './.next'],
-	// modulePathIgnorePatterns: ['./__tests__/api/utils'],
-	testMatch: ['**/**/__tests__/**/**/*.test.(ts)'],
-	setupFiles: ['dotenv/config'],
-	testTimeout: 7000
-}
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
 
-export default jestConfigs;
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
+
+export default config;
