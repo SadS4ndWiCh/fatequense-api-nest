191ce1b016d287e363596f8f68ab494fd80dfaf0
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-02 20:41:01 -0300

    fix toTitleCase helper function import

--- src/application/scrappers/profile.scrapper.ts
+++ src/application/scrappers/profile.scrapper.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { GXState } from '@helpers/gxstate';
-import { toTitleCase } from '@helpers/toTitleCase';
+import { toTitleCase } from '@helpers/to-title-case';
 
 @Injectable()
 export class ProfileScrapper {
