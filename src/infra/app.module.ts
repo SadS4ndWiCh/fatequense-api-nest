8414719f5453c1bd687865a1876a73c34cdeb93f
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-05 16:30:03 -0300

    add routes rate limiter

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+
 import { SigaModule } from './http/siga/siga.module';
 
 import jwtConfiguration from '@config/jwt.config';
@@ -11,6 +13,10 @@ import jwtConfiguration from '@config/jwt.config';
       cache: true,
     }),
     SigaModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 15,
+    }),
   ],
 })
 export class AppModule {}
