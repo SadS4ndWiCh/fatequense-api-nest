49c8d8e98309f4cdcfcef7f58380d5e5406dd71c
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-05 18:27:56 -0300

    chore: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './infra/app.module';
+import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
 
   app.useGlobalPipes(new ValidationPipe());
 
