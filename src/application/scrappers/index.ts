d8406399c13982ad0a6e7ae73f19371d9ba0ab2a
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-08 19:48:20 -0300

    chore: export all in an index file

---
+++ src/application/scrappers/index.ts
@@ -0,0 +1,15 @@
+import { ProfileScrapper } from '@application/scrappers/profile.scrapper';
+import { PartialGradeScrapper } from '@application/scrappers/partialGrade.scrapper';
+import { PartialAbsencesScrapper } from '@application/scrappers/partialAbsences.scrapper';
+import { HistoryScrapper } from '@application/scrappers/history.scrapper';
+import { ScheduleScrapper } from '@application/scrappers/schedule.scrapper';
+import { NoticesScrapper } from '@application/scrappers/notices.scrapper';
+
+export {
+  ProfileScrapper,
+  PartialAbsencesScrapper,
+  PartialGradeScrapper,
+  HistoryScrapper,
+  ScheduleScrapper,
+  NoticesScrapper,
+};
---
+++ src/application/use-cases/index.ts
@@ -0,0 +1,17 @@
+import { LoginWithSiga } from './login-with-siga';
+import { GetStudentProfile } from './get-student-profile';
+import { GetStudentPartialGrade } from './get-student-partial-grade';
+import { GetNotices } from './get-notices';
+import { GetStudentPartialAbsences } from './get-student-partial-absences';
+import { GetStudentHistory } from './get-student-history';
+import { GetStudentSchedule } from './get-student-schedule';
+
+export {
+  LoginWithSiga,
+  GetStudentProfile,
+  GetStudentPartialGrade,
+  GetNotices,
+  GetStudentPartialAbsences,
+  GetStudentHistory,
+  GetStudentSchedule,
+};
