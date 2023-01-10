9896138891d3ba57edd426a1f132cf600e99b09a
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-09 22:23:25 -0300

    moved

--- src/config/swagger.config.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentBuilder } from '@nestjs/swagger';
-
-export const swaggerConfig = new DocumentBuilder()
-  .setTitle('Fatequense API')
-  .setDescription(
-    'API para acessar informações de estudantes da faculdade Fatec',
-  )
-  .setVersion('v2.0.0')
-  .build();
