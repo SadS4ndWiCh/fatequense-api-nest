e494fd8a11c61f3967c1461727f4365f7e688125
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-09 22:12:46 -0300

    chore(swagger): download necessary swagger files to save statically

---
+++ src/swagger/download-swagger-files.ts
@@ -0,0 +1,31 @@
+import { createWriteStream } from 'node:fs';
+import { get } from 'node:http';
+
+const downloadFile = (url: string, filename: string): void => {
+  get(url, (res) => {
+    res
+      .pipe(
+        createWriteStream(`src/swagger/static/${filename}`, { flags: 'w+' }),
+      )
+      .once('close', () =>
+        console.log(
+          `[SWAGGER]: Swagger UI file written to: '/static/${filename}'`,
+        ),
+      );
+  });
+};
+
+export const createStaticSwagger = (port: string, path: string): void => {
+  const baseUrl = `http://localhost:${port}/${path}`;
+
+  const swaggerFilesToDownload = [
+    'swagger-ui-bundle.js',
+    'swagger-ui-init.js',
+    'swagger-ui-standalone-preset.js',
+    'swagger-ui.css',
+  ];
+
+  for (const filename of swaggerFilesToDownload) {
+    downloadFile(`${baseUrl}/${filename}`, filename);
+  }
+};
